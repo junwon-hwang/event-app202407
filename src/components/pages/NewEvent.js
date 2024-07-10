948d0ee39eecb853360b086a2f26f080186d12f4
junwon-hwnag <nyong1320@gmail.com>
2024-07-10 10:42:27 +0900

    first commit

---
+++ src/components/pages/NewEvent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import EventForm from '../components/EventForm';
+import { redirect } from 'react-router-dom';
+
+const NewEvent = () => {
+
+  return <EventForm method="post" />;
+};
+
+export default NewEvent;
+
