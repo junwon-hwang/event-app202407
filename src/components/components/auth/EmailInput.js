c53e2be8a939061ee14f610d5c11a307970b5037
junwon-hwnag <nyong1320@gmail.com>
2024-07-11 12:38:58 +0900

    # 이메일 중복 확인 요청

--- src/components/components/auth/EmailInput.js
+++ src/components/components/auth/EmailInput.js
@@ -1,5 +1,6 @@
 import React, { useEffect , useRef, useState } from 'react';
 import styles from './SignUpForm.module.scss';
+import { AUTH_URL } from '../../../config/host-config';
 
 const EmailInput = () => {
 
@@ -21,15 +22,25 @@ const EmailInput = () => {
   };
 
   // 이메일 검증 후속 처리 
-  const checkEmail = (email) => {
+  const checkEmail = async (email) => {
     if(!emailValid){
       // 에러메시지 세팅
       setError('이메일 형식이 유효하지 않습니다.');
       return;
     }
-    // 중복 검사 
+   
+     // 중복 검사 
+      const response = await fetch(`${AUTH_URL}/check-email?email=${email}`);
+      // console.log('res: ' , response);
+      const flag = await response.json();
 
-
+      // console.log('flag: ', flag);
+      if(flag){
+        setEmailValid(false);
+        setError('이메일이 중복되었습니다.');
+    }
+    
+    
   };
 
 
@@ -42,7 +53,7 @@ const EmailInput = () => {
     setEmailValid(isValid);
 
     // 이메일 검증 후속 처리
-    checkEmail();
+    checkEmail(email);
 
   };
 
