0c2310a945e663b473aaad54791a16bc2d262f95
junwon-hwnag <nyong1320@gmail.com>
2024-07-12 11:30:28 +0900

    # 패스워드 입력창 처리

--- src/components/components/auth/SignUpForm.js
+++ src/components/components/auth/SignUpForm.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './SignUpForm.module.scss';
 import EmailInput from './EmailInput';
 import VerificationInput from './VerificationInput';
 import ProgressBar from '../ProgressBar';
+import PasswordInput from './PasswordInput';
 
 const SignUpForm = () => {
 
@@ -13,27 +14,61 @@ const SignUpForm = () => {
   
   // 입력된 이메일
   const [enteredEmail, setEnteredEmail] = useState('');
+  // 입력된 패스워드
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [passwordIsValid, setPasswordIsValid] = useState(false);
+  // 회원가입 버튼 활성화 여부 
+  const [activeButton, setActiveButton] = useState(false);
 
-  // 이메일 중복확인이 끝났을 때 호출될 함수 
-  const emailSuccessHandler = (email) => {
-
+  // 다음 단계로 넘어가는 함수
+  const nextStep = () => {
+    // 다음 단계로 넘어가는 함수
     setSuccess (true);
-    setEnteredEmail(email);
 
     setTimeout(()=>{
-      setStep(2);
+      setStep(prevStep => prevStep + 1);
       setSuccess(false);
     },1500);
 
   };
 
+
+  // 이메일 중복확인이 끝났을 때 호출될 함수 
+  const emailSuccessHandler = (email) => {
+    setEnteredEmail(email);
+    nextStep();
+  };
+
+  const passwordSuccessHandler = (password,isVaild) => {
+    setEnteredPassword(password);
+    setPasswordIsValid(isVaild);
+  }
+
+
+  useEffect(()=>{
+    // 활성화 여부 감시
+    const isActive = enteredEmail && passwordIsValid;
+    setActiveButton(isActive);
+
+  },[enteredEmail, passwordIsValid])
+
+
   return (
     <div className='{styles.signuFrom}'>
       <div className='{styles.formStepActive}'>
         
-         {step === 1 && <EmailInput onSuccess={emailSuccessHandler}/>}
-         {step === 2 && <VerificationInput email={enteredEmail} />}
-
+         {step === 1 && <EmailInput  onSuccess={emailSuccessHandler}/>}
+         {step === 2 && <VerificationInput 
+            email = {enteredEmail} onSuccess={()=>nextStep()} />}
+         {step === 3 && <PasswordInput onSuccess={passwordSuccessHandler} />}
+         
+         {activeButton && 
+            <div>
+            <button>
+              회원가입 완료
+            </button>
+          </div>
+         }
 
          {success && <ProgressBar />}
       </div>
